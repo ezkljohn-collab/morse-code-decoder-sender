22b8e51d86a17878c5f05123e01de477caec73c7
ezkljohn-collab <ezkljohn@gmail.com>
2026-02-19 17:37:06 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,19 @@
+const CACHE_NAME = 'mcds-v2'; // Changed version to v2 to force update
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
 self.addEventListener('install', (e) => {
+  self.skipWaiting(); // Forces the new version to take over immediately
   e.waitUntil(
-    caches.open('mcds-v1').then((cache) => {
-      return cache.addAll([
-        'index.html',
-        'manifest.json'
-      ]);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
+    caches.match(e.request).then((response) => response || fetch(e.request))
   );
 });
